004d79c5ae24e7e73234fa77085a4ec312c018fb
sarthak_nawaria <sarthak.nawaria.41@gmail.com>
2023-06-20 23:07:45 +0530

    work started on "show all" tab

--- lib/views/ui/home_page.dart
+++ lib/views/ui/home_page.dart
@@ -97,7 +97,8 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                           Tab(
                             text: "Kids Shoes",
                           ),
-                        ]),
+                        ]
+                        ),
                   ],
                 ),
               ),
--- lib/views/ui/product_by_cat.dart
+++ lib/views/ui/product_by_cat.dart
@@ -1,4 +1,7 @@
+import 'package:antdesign_icons/antdesign_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/views/shared/app_style.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ProductByCat extends StatefulWidget {
   const ProductByCat({super.key});
@@ -11,21 +14,93 @@ class _ProductByCatState extends State<ProductByCat> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SizedBox(
-      child: Stack(
-        children: [
-          Container(
-            padding: const EdgeInsets.fromLTRB(16, 45, 0, 0),
-            height: MediaQuery.of(context).size.height * 0.4,
-            decoration: const BoxDecoration(
-              // image: DecorationImage(
-              //   image: AssetImage("assets/images/"),
-              //   fit:BoxFit.fill,
-              // ),
+      body: SizedBox(
+        child: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(16, 45, 0, 0),
+              height: MediaQuery.of(context).size.height * 0.4,
+              decoration: const BoxDecoration(
+                  // image: DecorationImage(
+                  //   image: AssetImage("assets/images/"),
+                  //   fit:BoxFit.fill,
+                  // ),
+
+                  ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(6, 12, 16, 18),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(
+                            AntIcons.closeOutlined,
+                            color: Colors.white,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(
+                            FontAwesomeIcons.sliders,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+            TabBar(
+                padding: EdgeInsets.zero,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorColor: Colors.transparent,
+                controller: _tabController,
+                isScrollable: true,
+                labelColor: Colors.white,
+                labelStyle: appstyle(24, Colors.white, FontWeight.bold),
+                unselectedLabelColor: Colors.grey.withOpacity(0.3),
+                tabs: const [
+                  Tab(
+                    text: "Mens Shoes",
+                  ),
+                  Tab(
+                    text: "Womens Shoes",
+                  ),
+                  Tab(
+                    text: "Kids Shoes",
+                  ),
+                ]),
+            TabBarView(
+              children: [
+                Container(
+                  color: Colors.green,
+                  height: 500,
+                  width: 300,
+                ),
+                Container(
+                  color: Colors.green,
+                  height: 500,
+                  width: 300,
+                ),
+                Container(
+                  color: Colors.green,
+                  height: 500,
+                  width: 300,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
